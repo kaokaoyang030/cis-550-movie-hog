b5db1592c20a3fe1a183206b2343e65f04b461b0
thomas koenig <tk2536@seas.upenn.edu>
2022-04-04 18:25:12 -0400

    favorites
    added favorites, like, unlike fcts

--- server/routes.js
+++ server/routes.js
@@ -24,11 +24,75 @@ async function hello(req, res) {
    }
 }
 
-async function all_matches(req, res) {
-   sql = `
-   Select movie_id
-   from cast_db
-   where cast_name = 'Tom Hanks';
+async function sign_up(req, res) {
+   const username = req.body.username;
+   const password = req.body.password;
+   if (!username || !password) {
+      res.send("Enter username and password!");
+      res.end();
+   }
+   let sql = `
+   insert into Users
+   value ('${username}', '${password}', 'NULL');
+   `;
+   connection.query(sql, function (error, results, fields) {
+      if (error) {
+         if (error.errno == 1062) {
+            //duplicate username
+            console.log(`Username already in use!`);
+            res.json({ valid: false });
+         } else {
+            console.log(error);
+            res.json({ error: error });
+         }
+      } else {
+         res.json({ valid: true });
+      }
+   });
+}
+
+async function sign_in(req, res) {
+   const username = req.body.username;
+   const password = req.body.password;
+   if (!username || !password) {
+      res.send("Enter username and password!");
+      res.end();
+   }
+   let sql = `
+   select *
+   from Users 
+   where username = '${username}'
+   and password = '${password}'
+   `;
+   connection.query(sql, function (error, results, fields) {
+      if (error) {
+         console.log(error);
+         res.json({ error: error });
+      } else {
+         const creds = JSON.parse(JSON.stringify(results));
+         if (creds.length == 1) {
+            req.session.username = username;
+            req.session.loggedin = true;
+            console.log(`Logged in as ${username}!`);
+            res.json({ valid: true });
+         } else {
+            console.log(`Wrong logging details!`);
+            res.json({ valid: false });
+         }
+      }
+   });
+}
+
+async function favorites(req, res) {
+   const username = req.body.username;
+   let where_clause = "";
+   if (username) {
+      where_clause = `where username = '${username}'`;
+   }
+   let sql = `
+   Select *
+   from Favorites
+   ${where_clause};
    `;
    connection.query(sql, function (error, results, fields) {
       if (error) {
@@ -40,7 +104,61 @@ async function all_matches(req, res) {
    });
 }
 
+async function like(req, res) {
+   const username = req.body.username;
+   const movie_id = parseInt(req.body.movie_id);
+   let sql = `
+    insert into Favorites
+    Value ('${username}', '${movie_id}', 'NULL')
+    `;
+   connection.query(sql, function (error, results, fields) {
+      if (error) {
+         if (error.errno == 1062) {
+            //duplicate username
+            console.log(`Movie already liked!`);
+            res.json({ valid: false });
+         } else if (error.errno == 1452) {
+            console.log("Movie id or username doesnt exist!");
+            res.json({ valid: false });
+         } else {
+            console.log(error);
+            res.json({ error: error });
+         }
+      } else {
+         res.json({ valid: true });
+      }
+   });
+}
+
+async function unlike(req, res) {
+   const username = req.body.username;
+   const movie_id = parseInt(req.body.movie_id);
+   let sql = `
+     delete from Favorites
+     where username = '${username}'
+     and movie_id = '${movie_id}'
+     `;
+   connection.query(sql, function (error, results, fields) {
+      if (error) {
+         console.log(error.errno);
+         if (error.errno == 1452) {
+            console.log("Movie id or username doesnt exist!");
+            res.json({ valid: false });
+         } else {
+            console.log(error);
+            res.json({ error: error });
+         }
+      } else {
+         res.json({ valid: true });
+      }
+   });
+}
+
 module.exports = {
    hello,
-   all_matches,
+   sign_up,
+   sign_in,
+   favorites,
+   like,
+   unlike,
 };
